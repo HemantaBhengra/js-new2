7fe327c2479d2192cefac771e3f5868788504ae4
HemantaBhengra <hemantabhengra8@gmail.com>
2025-05-07 10:33:59 +0000

    variables

---
+++ 01_new/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hemu1234@gamil.com"
+var accountPassword = "12345"
+accountCity = "burla"
+
+//accountId = 2
+//console.log(accountId);
+
+//node 01_new/01_variables.js//
+
+console.table([ accountId,accountEmail,accountPassword,accountCity])
